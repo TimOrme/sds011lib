cd50f47d3e2c0c2178c074c7e63c89d7cc1582de
Tim Orme <TimothyOrme@gmail.com>
2023-05-12 22:00:53 -0700

    Fix some comments

--- sds011lib/__init__.py
+++ sds011lib/__init__.py
@@ -50,12 +50,12 @@ class SerialLike(Protocol):
 
 @dataclass(frozen=True)
 class _RawReadResponse:
-    """Generic read response object for responses from SDS011.
+    """A raw read response object for responses from SDS011.
 
     Attributes:
         head: The header bytes from the response
         cmd_id: The command ID from the response
-        data: The data packet bytes from the response.
+        payload: The data packet bytes from the response.
         device_id: The device ID from the response
         checksum: The returned checksum for the response
         tail: The tail bytes from the response
@@ -65,7 +65,7 @@ class _RawReadResponse:
 
     head: bytes
     cmd_id: bytes
-    data: bytes
+    payload: bytes
     device_id: bytes
     checksum: int
     tail: bytes
@@ -380,7 +380,16 @@ class SDS011Reader:
         command_code: con.Command,
         response_type: con.ResponseType = con.ResponseType.GENERAL_RESPONSE,
     ) -> _RawReadResponse:
-        """Create a read response."""
+        """Parse bytes into a typed read response. Validates as well.
+
+        Args:
+            data: The raw data bytes to parse.  Must be of length 10.
+            command_code: The expected command code for the response.
+            response_type:  The expected response type for the response.
+
+        Returns:
+            A read response object with the parsed data.
+        """
         if len(data) != 10:
             raise IncompleteReadException()
 
@@ -395,7 +404,7 @@ class SDS011Reader:
         result = _RawReadResponse(
             head=head,
             cmd_id=cmd_id,
-            data=payload,
+            payload=payload,
             device_id=device_id,
             checksum=checksum,
             tail=tail,
@@ -406,71 +415,82 @@ class SDS011Reader:
         return result
 
     def _parse_query_response(self, data: bytes) -> QueryResponse:
-        """Create a query read response."""
+        """Parse a query read response."""
         raw_response = self._parse_read_response(
             data, con.Command.QUERY, con.ResponseType.QUERY_RESPONSE
         )
 
-        pm25: float = int.from_bytes(raw_response.data[2:4], byteorder="little") / 10
-        pm10: float = int.from_bytes(raw_response.data[4:6], byteorder="little") / 10
+        pm25: float = int.from_bytes(raw_response.payload[2:4], byteorder="little") / 10
+        pm10: float = int.from_bytes(raw_response.payload[4:6], byteorder="little") / 10
         return QueryResponse(pm25=pm25, pm10=pm10, device_id=raw_response.device_id)
 
     def _parse_reporting_mode_response(self, data: bytes) -> ReportingModeResponse:
-        """Create a reporting mode response."""
+        """Parse a reporting mode response."""
         raw_response = self._parse_read_response(
             data, command_code=con.Command.SET_REPORTING_MODE
         )
-        operation_type: con.OperationType = con.OperationType(raw_response.data[1:2])
-        state: con.ReportingMode = con.ReportingMode(raw_response.data[2:3])
+        operation_type: con.OperationType = con.OperationType(raw_response.payload[1:2])
+        state: con.ReportingMode = con.ReportingMode(raw_response.payload[2:3])
         return ReportingModeResponse(operation_type, state)
 
     def _parse_device_id_response(self, data: bytes) -> DeviceIdResponse:
-        """Create a device ID response."""
+        """Parse a device ID response."""
         raw_response = self._parse_read_response(
             data, command_code=con.Command.SET_DEVICE_ID
         )
         return DeviceIdResponse(device_id=raw_response.device_id)
 
     def _parse_sleep_wake_response(self, data: bytes) -> SleepWakeReadResponse:
-        """Create a sleep/wake response."""
+        """Parse a sleep/wake response."""
         raw_response = self._parse_read_response(
             data, command_code=con.Command.SET_SLEEP
         )
-        operation_type: con.OperationType = con.OperationType(raw_response.data[1:2])
-        state: con.SleepState = con.SleepState(raw_response.data[2:3])
+        operation_type: con.OperationType = con.OperationType(raw_response.payload[1:2])
+        state: con.SleepState = con.SleepState(raw_response.payload[2:3])
         return SleepWakeReadResponse(operation_type=operation_type, state=state)
 
     def _parse_working_period_reponse(self, data: bytes) -> WorkingPeriodReadResponse:
-        """Create a working period response."""
+        """Parse a working period response."""
         raw_response = self._parse_read_response(
             data, command_code=con.Command.SET_WORKING_PERIOD
         )
-        operation_type: con.OperationType = con.OperationType(raw_response.data[1:2])
-        interval: int = raw_response.data[2]
+        operation_type: con.OperationType = con.OperationType(raw_response.payload[1:2])
+        interval: int = raw_response.payload[2]
         return WorkingPeriodReadResponse(
             operation_type=operation_type, interval=interval
         )
 
     def _parse_firmware_response(self, data: bytes) -> CheckFirmwareResponse:
-        """Create a firmware response."""
+        """Parse a firmware response."""
         raw_response = self._parse_read_response(
             data, command_code=con.Command.CHECK_FIRMWARE_VERSION
         )
-        year: int = raw_response.data[1]
-        month: int = raw_response.data[2]
-        day: int = raw_response.data[3]
+        year: int = raw_response.payload[1]
+        month: int = raw_response.payload[2]
+        day: int = raw_response.payload[3]
         return CheckFirmwareResponse(year=year, month=month, day=day)
 
     def _verify(self, read_response: _RawReadResponse) -> None:
-        """Verify the read data."""
+        """Verify read data.
+
+        Args:
+            read_response: The raw read data to verify.
+
+        Raises:
+            IncorrectWrapperException: If the head or tail data is incorrect
+            ChecksumFailedException: If the checksum from the response is incorrect
+            IncorrectCommandException: If the command ID in the response is not the expected one.
+            IncorrectCommandCodeException: If the command code in the response is not the expected one.
+
+        """
         if read_response.head != con.HEAD:
             raise IncorrectWrapperException()
         if read_response.tail != con.TAIL:
             raise IncorrectWrapperException()
-        if read_response.checksum != self._calc_checksum(read_response.data):
+        if read_response.checksum != self._calc_checksum(read_response.payload):
             raise ChecksumFailedException(
                 expected=read_response.checksum,
-                actual=self._calc_checksum(read_response.data),
+                actual=self._calc_checksum(read_response.payload),
             )
         if read_response.cmd_id != read_response.expected_response_type.value:
             raise IncorrectCommandException(
@@ -481,12 +501,12 @@ class SDS011Reader:
         # Query responses don't validate the command code
         if (
             read_response.expected_response_type != con.ResponseType.QUERY_RESPONSE
-            and bytes([read_response.data[0]])
+            and bytes([read_response.payload[0]])
             != read_response.expected_command_code.value
         ):
             raise IncorrectCommandCodeException(
                 expected=read_response.expected_command_code.value,
-                actual=read_response.data[0:1],
+                actual=read_response.payload[0:1],
             )
 
     def _calc_checksum(self, data: bytes) -> int:
